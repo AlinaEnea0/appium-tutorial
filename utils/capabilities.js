3553a6d99ef303b0c34a074c52f15a889bbaf8f8
AlinaEnea20 <alina.en@moonactive-bp.com>
2025-05-17 22:21:13 +0300

    Initial commit - Add configuration

---
+++ utils/capabilities.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const getPort = (base, index) => base + index;
+
+const capabilities = [
+     {
+            "platformName": 'iOS',
+            'appium:deviceName': 'iPhone 16',
+            'appium:platformVersion': '18.3',
+            'appium:automationName': 'XCUITest',
+            "wdio:capabilities": {
+                platform: 'ios'
+            },
+             "appium:app" : "path/to/your/app.app",
+             "appium:derivedDataPath": "/Users/alinaenea/Library/Developer/Xcode/DerivedData",
+            "appium:usePrebuiltWDA": true, 
+            "appium:showXcodeLog": true, 
+          }, 
+          {
+            "platformName": "Android",
+            "appium:deviceName": "Medium_Phone_API_35",
+            "appium:platformVersion": "15.0",
+            "appium:automationName": "UiAutomator2",
+            "appium:app": path.join(__dirname, '..', 'app', 'debug-app.apk'), // or if you don't have an apk in a specific directory like "app" , write just "path/to/your/app.apk"
+            "wdio:capabilities": {
+                platform: 'android'
+            }
+        }
+];
+module.exports = capabilities;
+
---
+++ wdio.conf.js
@@ -0,0 +1,34 @@
+ 
+const path = require('path');
+const argv = require('yargs').argv;
+const testCapabilities = require('./utils/capabilities');
+
+exports.config = {
+    runner: 'local',
+    specs: [path.join(__dirname, './tests/spec/**/*.js')],
+    maxInstances: 2, 
+    capabilities: testCapabilities.filter(capability => {
+        const platform = argv.platform;
+        if (platform) {
+            return capability['wdio:capabilities'].platform === platform;
+        }
+        return true;
+    }),
+
+    // Appium Configuration
+    services: ['appium'], //open Appium server 
+    port: 4724, // default port for Appium server
+    path: '/wd/hub',
+    logLevel: 'error',
+
+    // Framework and Hooks
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+ 
+    reporters: ['spec'],
+ 
+    waitforTimeout: 15000,
+}
